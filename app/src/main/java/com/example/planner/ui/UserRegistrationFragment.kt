f31fc66c4fa989fcafc24bc185f43abb89e92fa8
Ana Letícia César de Oliveira <analeticia_cesar@hotmail.com>
2025-05-08 19:43:40 -0300

    Refactor code.

--- app/src/main/java/com/example/planner/ui/UserRegistrationFragment.kt
+++ app/src/main/java/com/example/planner/ui/UserRegistrationFragment.kt
@@ -1,11 +1,10 @@
 package com.example.planner.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.planner.R
+import androidx.fragment.app.Fragment
 import com.example.planner.databinding.FragmentUserRegistrationBinding
 
 class UserRegistrationFragment : Fragment() {
@@ -25,7 +24,7 @@ class UserRegistrationFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         with(binding) {
-            // TODO: logica de tela de cadastro do usuário
+            // TODO: user registration screen logic
         }
     }
 
